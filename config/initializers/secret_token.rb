694c2fa09b2b244ca51732bbcf3cd5f305b63253
push2pull <james.roitberg@gmail.com>
2012-01-11 15:38:46 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Junior::Application.config.secret_token = 'b1b6518d160afdd539329db9dfdb532435d24586bc014a1a49b2d9e3d1a5c548a614618a168f3db4ef94def70c0512fcfece9fcef9a3f7dd36c4438727ac6b21'
